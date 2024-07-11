0ac61ebe4d368623e4c7f72f62efe35a25f08f5a
{ Walyson } <walysonmoura222@gmail.com>
2024-07-11 23:05:57 +0000

    chore(project): add ESLint configuration files (.eslintrc.js, .eslintignore)
    Added .eslintrc.js to configure ESLint for the project and .eslintignore to specify files and
    directories to ignore for ESLint checks. These configurations ensure consistent code style and
    quality across the project.

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@react-native-community",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["simple-import-sort", "prettier", "unicorn", "import"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        endOfLine: "auto",
+      },
+    ],
+    "unicorn/filename-case": [
+      "warn",
+      {
+        case: "kebabCase",
+        ignore: ["/android", "/ios"],
+      },
+    ],
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/no-duplicates": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+  },
+};
